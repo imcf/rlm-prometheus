af2d39c14a7ce4b4aa4c7e6ce0557e72eab4cb89
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2022-11-21 21:01:02 +0100

    Fix get_config_from_env(), add 'isv' setting

--- src/rlm_prometheus/config.py
+++ src/rlm_prometheus/config.py
@@ -11,16 +11,20 @@ def load_config_file(filename):
 
 def get_config_from_env():
     """Assemble a config object from envrionment variables."""
-    return Box({"exporter_port": 8909, "rlm_host": "local", "rlm_port": 5054})
-
     try:
-        rlm_host = os.environ["RLM_HOST"]
+        isv = os.environ["RLM_ISV"]
     except Exception as err:
         raise KeyError("Error getting configuration values:", err) from err
 
+    rlm_host = os.environ.get("RLM_HOST", "localhost")
     rlm_port = os.environ.get("RLM_PORT", "5054")
     exporter_port = os.environ.get("RLM_PORT", "8909")
 
     return Box(
-        {"rlm_port": rlm_port, "rlm_host": rlm_host, "exporter_port": exporter_port}
+        {
+            "rlm_port": rlm_port,
+            "rlm_host": rlm_host,
+            "exporter_port": exporter_port,
+            "isv": isv,
+        }
     )

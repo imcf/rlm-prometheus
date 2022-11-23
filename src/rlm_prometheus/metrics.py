178cb5ab5d767897a7e1c6a889b3ab9c7d2c5bdf
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2022-11-23 16:03:40 +0100

    Account for missing table when no licenses are in use

--- src/rlm_prometheus/metrics.py
+++ src/rlm_prometheus/metrics.py
@@ -32,7 +32,19 @@ class RlmProductMetrics:
         except Exception as err:  # pylint: disable-msg=broad-except
             raise RuntimeError(f"Fetching new data failed: {err}") from err
 
-        for _, row in tables[1].iterrows():
+        # in case no licenses are in use the returned data will only contain
+        # one single table, otherwise it should have two:
+        count = len(tables)
+        if count == 1:
+            pool_status = tables[0]
+        elif count == 2:
+            pool_status = tables[1]
+        else:
+            raise ValueError(
+                f"Cannot parse data, found {count} tables - expected 1 or 2!"
+            )
+
+        for _, row in pool_status.iterrows():
             product = row["Product"]
             if "imarisreader" in product:
                 continue  # ignore readers

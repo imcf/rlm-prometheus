a64e955070634378a53712fc386c80a4e5309530
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2022-11-23 15:35:01 +0100

    Port needs to be an int, not str

--- src/rlm_prometheus/config.py
+++ src/rlm_prometheus/config.py
@@ -10,7 +10,7 @@ def load_config_file(filename):
     if "rlm_uri" not in config.keys():
         config.rlm_uri = "http://localhost:5054"
     if "exporter_port" not in config.keys():
-        config.exporter_port = "8909"
+        config.exporter_port = 8909
     if "interval" not in config.keys():
         config.interval = 60
     if "isv" not in config.keys():
@@ -26,7 +26,7 @@ def get_config_from_env():
         raise KeyError("Error getting configuration values:", err) from err
 
     rlm_uri = os.environ.get("RLM_URI", "http://localhost:5054")
-    exporter_port = os.environ.get("RLM_EXPORTER_PORT", "8909")
+    exporter_port = os.environ.get("RLM_EXPORTER_PORT", 8909)
     interval = os.environ.get("RLM_EXPORTER_INTERVAL", 60)
 
     return Box(
